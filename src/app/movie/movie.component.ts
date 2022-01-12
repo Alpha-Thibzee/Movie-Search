44762e4385dbc23293040d727c363ff0e1ca5d7d
ArnaudDevWeb <arnaud.albouycci@gmail.com>
2022-01-12 08:59:24 +0100

    Barre de recherche film

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
+  movie : string = 'Die Hard';
+  movieData : any;
   constructor() { }
 
   ngOnInit(): void {
+    this.getMovieData();
   }
+  getMovieData() : void {
+    fetch(`http://www.omdbapi.com/?apikey=2f2d427&s=${this.movie}`)
+    
+    .then(response=>response.json())
+    //.then(data=>console.log(data));
+    .then(data=>this.setMovieData(data));
+  }
+ 
+  setMovieData(data:any) : void {
 
-}
+    //this.movieData=data;
+    this.movieData=data.Search;
+    
+    //this.movieData.imdbID = (this.movieData.imdbID);
+    this.movieData.Title = (this.movieData.Title);
+    this.movieData.Country = (this.movieData.Country);
+    this.movieData.Year = (this.movieData.Year);
+    this.movieData.Poster = (this.movieData.Poster);
+  
+  
+  
+  
+  }
+    
+    
+
+    searchMovie() : void{
+      this.getMovieData();
+    }
+} 
+//
